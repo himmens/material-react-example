18a5ea2b7a61f992990db34da494768d4cdea3e6
s.himanych <s.himanych@zmeke.com>
2018-07-17 00:04:35 +0300

    initial project setup with material-ui button

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Button from '@material-ui/core/Button';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <Button variant="contained" color="primary" classes="">
+          Hello World
+        </Button>
+      </div>
+    );
+  }
+}
